7fa0d68686255eb88fbcb8d0f569a197b94f40cb
BennyVong <superbenny123@gmail.com>
2020-04-08 19:36:45 -0700

    Update create_tables_mysql.py

--- create_tables_mysql.py
+++ create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="MSI", user="user", password="password", database="readings")
+db_conn = mysql.connector.connect(host="ec2-52-25-67-56.us-west-2.compute.amazonaws.com", user="user", password="password", database="readings")
 
 db_cursor = db_conn.cursor()
 
